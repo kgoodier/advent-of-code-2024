1fd91ddb911b684ab92f83ba9919b96138cfb9b9
Kevin Goodier <kevin@biggreenball.com>
2024-12-02 21:01:20 -0800

    Day 2 part 2
    really annoying
    - Go modifies underlying slices
    - first and last element deletion annoying
    
    ended up just brute forcing it

--- day-2/main.go
+++ day-2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc24/elves"
 	"fmt"
+	"slices"
 )
 
 func main() {
@@ -11,28 +12,70 @@ func main() {
 		panic(err)
 	}
 
+	p1(data)
+	p2(data)
+}
+
+func p1(data [][]int) {
 	safeReports := 0
 	for _, report := range data {
-		prev := 0
-		safe := true
-		for i := 0; i < len(report)-1; i++ {
-			d := report[i+1] - report[i]
-			if d < 0 && prev <= 0 && d >= -3 {
-				prev = d
-				continue
-			} else if d > 0 && prev >= 0 && d <= 3 {
-				prev = d
-				continue
-			} else {
-				safe = false
-				break
-			}
-		}
-
-		if safe {
+		ok, _ := checkReport(report)
+		if ok {
 			safeReports++
 		}
 	}
 
 	fmt.Println(safeReports)
+}
+
+func p2(data [][]int) {
+	safeReports := 0
+	for _, report := range data {
+		ok, _ := checkReport(report)
+		if ok {
+			safeReports++
+			continue
+		}
+
+		if checkWithDeletion(report) {
+			safeReports++
+			continue
+		}
+	}
+
+	fmt.Println(safeReports)
+}
+
+func checkWithDeletion(report []int) bool {
+	for i := 0; i < len(report); i++ {
+		reportLeft := deleteElement(report, i)
+		ok, _ := checkReport(reportLeft)
+		if ok {
+			return true
+		}
+	}
+	return false
+}
+
+func deleteElement(s []int, i int) []int {
+	c := slices.Clone(s)
+	return slices.Delete(c, i, i+1)
+}
+
+func checkReport(report []int) (bool, int) {
+	prev := 0
+	for i := 0; i < len(report)-1; i++ {
+		d := report[i+1] - report[i]
+		if d < 0 && prev <= 0 && d >= -3 {
+			prev = d
+			continue
+		} else if d > 0 && prev >= 0 && d <= 3 {
+			prev = d
+			continue
+		} else {
+			return false, i
+		}
+	}
+
+	return true, 0
 }
