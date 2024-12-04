72e04b1347b4306748eacad85096610e2df48af4
Kevin Goodier <kevin@biggreenball.com>
2024-12-03 19:39:26 -0800

    Day 3 part 2
    Annoying regex, made some assumptions

--- day-3/main.go
+++ day-3/main.go
@@ -16,6 +16,11 @@ func main() {
 
 	data := strings.TrimSpace(string(bytes))
 
+	part1(data)
+	part2(data)
+}
+
+func part1(data string) {
 	// Regular expression to match valid mul(X,Y) instructions
 	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
 
@@ -35,5 +40,43 @@ func main() {
 		sum += x * y
 	}
 
-	fmt.Println("Sum of all multiplications:", sum)
+	fmt.Println("[Part 1] Sum of all multiplications:", sum)
+}
+
+func part2(data string) {
+	// Regular expression to match valid mul(X,Y) instructions
+	re := regexp.MustCompile(`(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))`)
+
+	// Find all matches
+	matches := re.FindAllStringSubmatch(data, -1)
+
+	sum := 0
+	multEnabled := true
+
+	for _, match := range matches {
+		// Extract the name and numbers
+		cmd := match[1]
+
+		switch {
+		case cmd == "do()":
+			multEnabled = true
+			fmt.Println("Enabled multiplication")
+		case cmd == "don't()":
+			multEnabled = false
+			fmt.Println("Disabled multiplication")
+		case strings.HasPrefix(cmd, "mul("):
+			if multEnabled {
+				x, err1 := strconv.Atoi(match[2])
+				y, err2 := strconv.Atoi(match[3])
+				if err1 != nil || err2 != nil {
+					continue
+				}
+				sum += x * y
+			}
+		default:
+			panic("Invalid command " + cmd)
+		}
+	}
+
+	fmt.Println("[Part 2] Sum of all multiplications:", sum)
 }
