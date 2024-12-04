114a0b974e770c5db28dc7f146cc2c68f96f04f0
Kevin Goodier <kevin@biggreenball.com>
2024-12-04 07:44:58 -0800

    Day 4 part 2

--- day-4/main.go
+++ day-4/main.go
@@ -17,12 +17,17 @@ func main() {
 		panic(err)
 	}
 
-	p := Puzzle{
+	p := &Puzzle{
 		lines:  lines,
 		width:  len(lines[0]),
 		height: len(lines),
 	}
 
+	part1(p)
+	part2(p)
+}
+
+func part1(p *Puzzle) {
 	total := 0
 	word := "XMAS"
 	for y := 0; y < p.height; y++ {
@@ -39,6 +44,38 @@ func main() {
 	}
 
 	fmt.Println("Part 1: ", total)
+}
+
+func part2(p *Puzzle) {
+	// Looking for 2 diagonal "MAS" with the A overlapping
+	total := 0
+	word := "MAS"
+	for y := 0; y < p.height; y++ {
+		for x := 0; x < p.width; x++ {
+			c1 := 0
+			c2 := 0
+
+			// We're working on a 3x3 grid, so we have to nudge the starting point
+			// of the searches out to the corners
+
+			// / diagonal
+			c1 += p.search(word, x-1, y+1, 1, -1) // right up
+			c1 += p.search(word, x+1, y-1, -1, 1) // left down
+
+			if c1 == 1 {
+				// \ diagonal
+				c2 += p.search(word, x-1, y-1, 1, 1)   // right down
+				c2 += p.search(word, x+1, y+1, -1, -1) // left up
+			}
+
+			if c1 == 1 && c2 == 1 {
+				total++
+			}
+		}
+	}
+
+	fmt.Println("Part 2: ", total)
+
 }
 
 func (p *Puzzle) get(x, y int) byte {
